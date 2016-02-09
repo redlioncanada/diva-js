4657a8d1ae94c2151ff5664614e0a838682a4cc8
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-09 17:42:59 -0500

    Added boundary offset

--- app/diva.js
+++ app/diva.js
@@ -7,7 +7,7 @@ var diva = (function() {
 		for (var i in evts) {
 			var evt = evts[i];
 			if ((evt.total == -1 || evt.count < evt.total)) {
-				if (elementIsInView(evt.element)) {
+				if (elementIsInView(evt.element, evt.offset)) {
 					if (!evt.triggered) {
 						evt.callback.call(self,evt);
 						evt.triggered = true;
@@ -24,22 +24,40 @@ var diva = (function() {
 		}
 	}
 
-	function elementIsInView(element) {
-		var el = getElementPosition(element);
+	function elementIsInView(element, offset) {
+		var el = getElementPosition(element), percentOffset = false;
 		el.width = element.offsetWidth;
 		el.height = element.offsetHeight;
 
-		var screenUpperBound = window.scrollY;
-		var screenLowerBound = window.scrollY + Math.min(window.screen.height, window.innerHeight, window.outerHeight);
+		var windowHeight = Math.min(window.screen.height, window.innerHeight, window.outerHeight);
+		var windowWidth = Math.min(window.screen.width, window.innerWidth, window.innerHeight);
+		offset = computeBoundsOffset(offset, windowHeight, windowWidth);
+		var screenUpperBound = window.scrollY + offset;
+		var screenLowerBound = window.scrollY + windowHeight - offset;
 		var elementUpperBound = el.top;
 		var elementLowerBound = el.top + el.height;
-		var screenLeftBound = window.scrollX;
-		var screenRightBound = window.scrollX + Math.min(window.screen.width, window.innerWidth, window.innerHeight);
+		var screenLeftBound = window.scrollX + offset;
+		var screenRightBound = window.scrollX + windowWidth - offset;
 		var elementLeftBound = el.left;
 		var elementRightBound = el.left + el.width;
 
 		return ((elementUpperBound <= screenUpperBound && elementUpperBound < screenLowerBound && elementLowerBound > screenLowerBound && elementLowerBound > screenUpperBound) || (elementUpperBound >= screenUpperBound && elementUpperBound < screenLowerBound && elementLowerBound > screenLowerBound) || (elementLowerBound >= screenUpperBound && elementLowerBound < screenLowerBound && elementUpperBound < screenUpperBound)) && 
 				((elementLeftBound <= screenLeftBound && elementLeftBound < screenRightBound && elementRightBound > screenRightBound && elementRightBound > screenLeftBound) || (elementLeftBound >= screenLeftBound && elementLeftBound < screenRightBound && elementRightBound > screenRightBound) || (elementRightBound >= screenLeftBound && elementRightBound < screenRightBound && elementLeftBound < screenLeftBound));
+	}
+
+	function computeBoundsOffset(offset, windowHeight) {
+		if (!offset) return 0;
+		if (typeof offset === 'number') return offset;
+		if (typeof offset !== 'string') return 0;
+		if (offset.indexOf('%') > -1) percentOffset = true;
+		else percentOffset = false;
+
+		offset = parseFloat(offset.replace(/%|px/g, ''));
+		if (isNaN(offset)) offset = 0;
+		if (percentOffset) offset = windowHeight*offset;
+		if (offset > 100) offset = 100;
+		if (offset < 0) offset = 0;
+		return offset;
 	}
 
 	function getElementPosition(el) {
@@ -66,7 +84,12 @@ var diva = (function() {
 	    return {top:curtop, left:curleft};
 	}
 
-	function addEvent(selector, count, callback) {
+	function addEvent(selector, opts, callback) {
+		if (typeof opts === 'function') {
+			callback = opts;
+			opts = {};
+		}
+
 		var element = selector;
 		if (typeof element === 'string') {
 			if (element.indexOf('#') > -1 || element.indexOf('.') == -1) {
@@ -76,13 +99,14 @@ var diva = (function() {
 				element = element.replace(/\.|#/g, '');
 				element = document.getElementsByClassName(element)[0];
 			}
-		} 
+		}
 		if (!element || element === null || typeof callback !== 'function') return;
 		evts.push({
 			selector: typeof selector === 'string' ? selector : element,
 			element: element,
 			count: 0,
-			total: count,
+			offset: opts.offset ? opts.offset : 0,
+			total: opts.count ? opts.count : -1,
 			callback: callback,
 			triggered: false
 		});
@@ -109,14 +133,14 @@ var diva = (function() {
 
 	return {
 		events: evts,
-		once: function(selector, callback) {
-			addEvent(selector, 1, callback);
+		once: function(selector, opts, callback) {
+			addEvent(selector, opts, callback);
 		},
-		always: function(selector, callback) {
-			addEvent(selector, -1, callback);
+		always: function(selector, opts, callback) {
+			addEvent(selector, opts, callback);
 		},
-		sometimes: function(selector, num, callback) {
-			addEvent(selector, num, callback);
+		sometimes: function(selector, opts, callback) {
+			addEvent(selector, opts, callback);
 		},
 		destroy: function(element) {
 			if (typeof element === 'string' || typeof element === 'object') removeEvent(element);
